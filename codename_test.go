c9ffaead72b67facb0c9149704a881d58126b4e3
Jonathan Gautheron <jgautheron@nexway.com>
2015-08-29 22:31:19 +0200

    test(codename): remove goconvey

--- codename_test.go
+++ codename_test.go
@@ -4,20 +4,25 @@ import (
 	"testing"
 
 	"github.com/jgautheron/codename-generator"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestCodenameGeneration(t *testing.T) {
 	var cn string
 	var err error
 
-	Convey("Codename should be generated", t, func() {
-		cn, err = codename.Get(codename.Sanitized)
-		So(err, ShouldBeNil)
-		So(len(cn), ShouldBeGreaterThan, 3)
+	cn, err = codename.Get(codename.Sanitized)
+	if err != nil {
+		t.Error("The codename was not generated")
+	}
+	if len(cn) < 3 {
+		t.Error("The codename generated is invalid")
+	}
 
-		cn, err = codename.Get(codename.SpacedString)
-		So(err, ShouldBeNil)
-		So(len(cn), ShouldBeGreaterThan, 3)
-	})
+	cn, err = codename.Get(codename.SpacedString)
+	if err != nil {
+		t.Error("The codename was not generated")
+	}
+	if len(cn) < 3 {
+		t.Error("The codename generated is invalid")
+	}
 }
